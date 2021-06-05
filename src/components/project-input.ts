d47d463d506fcba5bb46071f861b2c13953be878
shivamgupta20 <shivamgupta20@gmail.com>
2021-06-05 16:27:05 +0530

    adding webpack build tool

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
-import { autoBind } from "../decorators/autoBind.js";
-import { projectState } from "../state/project-state.js";
-import { Validatable, validate } from "../util/validation.js";
-import { Component } from "./base-component.js";
+import { autoBind } from "../decorators/autoBind";
+import { projectState } from "../state/project-state";
+import { Validatable, validate } from "../util/validation";
+import { Component } from "./base-component";
 
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
     titleInputElement: HTMLInputElement;
